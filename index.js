ef1838675224307f8d97e141b9b94656edf7ace1
Mahidi-Hasan-Supon <mahidihasansupon@gmail.com>
2025-09-09 13:44:44 +0600

    adde

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const categorieContainer = document.getElementById('categorie-container');
 const cardContainer = document.getElementById('card-container');
+const plantsContainer = document.getElementById('plants-container')
 
 
 
 
 const loadcategorie = ()=>{
+    
     fetch('https://openapi.programming-hero.com/api/categories')
     .then(res=>res.json())
     .then(data=>{
-        loadAllPlants()
-        
+       
         displayloadcategorie(data.categories)
     })
     .catch((err)=>{
@@ -17,9 +18,18 @@ const loadcategorie = ()=>{
     })
 }
 
-// card click 
+// hidean click 
 
+const hiddenbtn = (status)=>{
+    if(status == true) {
+        document.getElementById('hidden').classList.remove('hidden')
+        document.getElementById('plants-container').classList.add('hidden')
+    }else{
+        document.getElementById('plants-container').classList.remove('hidden')
+        document.getElementById('hidden').classList.add('hidden')
 
+    }
+}
 
 
 
@@ -33,6 +43,9 @@ const displayloadcategorie=(categories)=>{
         </div>
         `
     }
+
+}
+
     categorieContainer.addEventListener('click',(e)=>{
        
 
@@ -47,16 +60,17 @@ const displayloadcategorie=(categories)=>{
             
         }
     })
-}
+
 
 const loadByCardCategorie = (id)=>{
+    
     // console.log(plantsId)
     const url = `https://openapi.programming-hero.com/api/category/${id}`;
     fetch(url)
     .then(res=>res.json())
     .then(data=>{
         console.log(data)
-        loadAllPlants()
+        // loadAllPlants()
 
         showByCardCategorie(data.plants)
     })
@@ -76,13 +90,14 @@ const showByCardCategorie = (plants)=>{
 // name: "Champa"
 // price: 300
 
-
+ plantsContainer.innerHTML=''
 
     for(let plant of plants){
         console.log(plant)
-        cardContainer.innerHTML +=`
+       
+        plantsContainer.innerHTML +=`
          
-           <div class="bg-white rounded-xl  p-5 rounded-lg">
+           <div  class="bg-white rounded-xl  p-5 rounded-lg">
             <div >
             <img src="${plant.image}" class="rounded-xl w-full h-80" alt="">
             <h1 class=" text-sm font-bold my-2 ">${plant.name}</h1>
@@ -105,15 +120,34 @@ const loadAllPlants =()=>{
     fetch(url)
     .then(res=>res.json())
     .then(data=>{
-    displayAllPlants(data.allplans)
+    displayAllPlants(data.plants)
   })
 }
 
-const displayAllPlants = (allplans)=>{
-    console.log(allplans)
+const displayAllPlants = (trees)=>{
+    
+    trees.forEach(tree=>{
+        
+        plantsContainer.innerHTML+=`
+         <div class="bg-white rounded-xl  p-5 rounded-lg">
+            <div class="">
+            <img src="${tree.image}" class="rounded-xl w-full h-80" alt="">
+            <h1 class=" text-sm font-bold my-2 ">${tree.name}</h1>
+            <p class="text-sm ">${tree.description}</p>
+            <div class="flex my-3 justify-between ">
+                <button class="btn bg-[#DCFCE7] text-[#15803D] rounded-4xl ">Fruit Tree</button>
+                <p><i class="fa-solid fa-bangladeshi-taka-sign"></i><span>${tree.price}</span></p>
+            </div>
+            <button class="btn rounded-3xl w-full text-white bg-[#15803D]">Add to Cart</button>
+
+            </div>
+        </div>
+        
+        `
+    
+    })
 
 }
-
 
 loadAllPlants()
 
